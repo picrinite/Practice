7f17b91b47e8761b96ca96a70b003ed9c0782037
picrinite <picrinite@users.noreply.github.com>
2016-08-18 10:38:45 -0400

    Improvement

--- Catalan_Number-Stack_In_and_Out.cpp
+++ Catalan_Number-Stack_In_and_Out.cpp
@@ -1,5 +1,25 @@
 #include <cstdio>
 #include <iostream>
+
+// numIn is # in the stack, numLeft is # to be put in stack
+int calCatalan(int numIn, int numLeft, int (*dict)[100]) {
+	if (numLeft == 0)
+		return 1;
+	if (dict[numIn][numLeft])
+		return dict[numIn][numLeft];
+	int res = calCatalan(numIn + 1, numLeft - 1, dict) + (numIn >= 1 ? calCatalan(numIn - 1, numLeft, dict) : 0);
+	dict[numIn][numLeft] = res;
+	return res;
+}
+int main() {
+	int num;
+	scanf("%d", &num);
+	int dict[100][100] = { {} };
+	std::cout << calCatalan(0, num, dict) << std::endl;
+}
+
+// worse implementation
+/*
 int calCatalan(int numIn, int numLeft) {
 	if (numLeft == 0)
 		return 1;
@@ -10,3 +30,4 @@ int main() {
 	scanf("%d", &num);
 	std::cout << calCatalan(0, num) << std::endl;
 }
+*/
